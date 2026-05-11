b26d02d80fce11f6a6d61c9d4d75b4c09b423b7e
Evyn Rissling <evyn710@gmail.com>
2026-05-10 22:35:50 -0600

    Have working mandelbrot image now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use iced::{Element};
 use iced::widget;
-use iced::Subscription;
+use iced::{Point, Subscription, Size};
 use iced::advanced::image;
+use num::complex::Complex;
+use std::time::Instant;
 
 fn main() -> iced::Result {
     iced::application(
@@ -39,14 +41,46 @@ impl MandelbrotViewer {
 
 fn mandelbrot_slow() -> image::Handle {
     let mut pixels: Vec<u8> = Vec::new();
-    for row in 0..512 {
-        for column in 0..512 {
-            let brightness_value: u8 = (row as f32 * column as f32 / (512.0 * 512.0) * 255.0) as u8;
-            pixels.push(brightness_value); 
-            pixels.push(brightness_value); 
-            pixels.push(brightness_value); 
+    let image_size  = Size::new(900, 600);
+    let set_size = Size::new(3, 2);
+    let top_left = Point::new(-2, 1);
+    let max_iterations = 1000;
+    let x_scale: f64 = set_size.width as f64 / image_size.width as f64;
+    let y_scale: f64 = set_size.height as f64 / image_size.height as f64;
+
+    let start = Instant::now();
+    for row in 0..image_size.height {
+        for column in 0..image_size.width {
+            let mut colour = 255;
+            let x_position: f64 = top_left.x as f64 + x_scale * column as f64;
+            let y_position: f64 = top_left.y as f64 - y_scale * row as f64;
+            let c = Complex::new(x_position, y_position);
+
+            let mut iteration = 0;
+            let mut z: Complex<f64> = Complex::new(0.0, 0.0);
+            while z.norm_sqr() <= 4.0 && iteration < max_iterations {
+                z = z * z + c;
+                iteration += 1;
+            }
+
+            if iteration == max_iterations {
+                colour = 0;
+            }
+
+            // println!("C: {c}");
+            // println!("Iterations: {iteration}");
+            // println!("Colour: {colour}");
+
+            pixels.push(colour); 
+            pixels.push(colour); 
+            pixels.push(colour); 
             pixels.push(255);
+
         }
     }
-    image::Handle::from_rgba(512, 512, pixels)
+
+    let duration = start.elapsed();
+    println!("{:?}", duration);
+    
+    image::Handle::from_rgba(image_size.width, image_size.height, pixels)
 }
